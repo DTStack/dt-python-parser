f5da81a7954997f5298a5e27054c909c2a33c7e0
chenfeng <chenfeng@dtstack.com>
2020-11-04 18:49:35 +0800

    build: update version

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,2 +1,22 @@
-export * from './python2'
-// export * from './python3'
+import Python2 from './python2'
+
+// import Python3 from './python3'
+
+// export const Python3Parser = new Python3()
+export const ParserPython2 = (stringArray) => {
+  let resultArray=[]
+  const Python2Parser = new Python2()
+  // try{
+    stringArray.forEach(sentenceList=>{
+      sentenceList.forEach(sentence=>{
+        const result = Python2Parser.validate(sentence)
+        resultArray.concat(result)
+      } )
+    })
+  // }
+  // catch(e){
+  //   throw new Error(e)
+  // }
+  console.log('我是神！',resultArray)
+  return {expected:resultArray}
+}
