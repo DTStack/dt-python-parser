fba7f81055c94f85766ef48c0538cac446fb0676
chenfeng <chenfeng@dtstack.com>
2020-11-09 16:54:45 +0800

    feat: add python3 parser entry

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,8 +1,6 @@
 import Python2 from './python2'
+import Python3 from './python3'
 
-// import Python3 from './python3'
-
-// export const Python3Parser = new Python3()
 export const ParserPython2 = (stringArray) => {
   let resultArray
   const Python2Parser = new Python2()
@@ -12,10 +10,15 @@ export const ParserPython2 = (stringArray) => {
   }
   else{resultArray=stringArray}
   const result = Python2Parser.validate(resultArray)
-  console.log(result)
-  // }
-  // catch(e){
-  //   throw new Error(e)
-  // }
+  return result[0]
+}
+export const ParserPython3 = (stringArray) => {
+  let resultArray
+  const Python3Parser = new Python3()
+  if(Array.isArray(stringArray)) {
+    resultArray=stringArray.join('')
+  }
+  else{resultArray=stringArray}
+  const result = Python3Parser.validate(resultArray)
   return result[0]
 }
