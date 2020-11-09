65bc7a406b9d526a5c6a6594c265b22b2ca9086b
chenfeng <chenfeng@dtstack.com>
2020-11-09 10:22:43 +0800

    feat: resource processing portal

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -4,19 +4,18 @@ import Python2 from './python2'
 
 // export const Python3Parser = new Python3()
 export const ParserPython2 = (stringArray) => {
-  let resultArray=[]
+  let resultArray
   const Python2Parser = new Python2()
-  // try{
-    stringArray.forEach(sentenceList=>{
-      sentenceList.forEach(sentence=>{
-        const result = Python2Parser.validate(sentence)
-        resultArray.concat(result)
-      } )
-    })
+  if(Array.isArray(stringArray)) {
+    resultArray=stringArray.join('')
+
+  }
+  else{resultArray=stringArray}
+  const result = Python2Parser.validate(resultArray)
+  console.log(result)
   // }
   // catch(e){
   //   throw new Error(e)
   // }
-  console.log('我是神！',resultArray)
-  return {expected:resultArray}
+  return result[0]
 }
