3891230133b2ee319a1ce52d7d56aa541815213f
chenfeng <chenfeng@dtstack.com>
2020-11-09 19:36:05 +0800

    test: fix wrong test result

--- test/parser/python2/listener.test.ts
+++ test/parser/python2/listener.test.ts
@@ -1,21 +1,25 @@
 import Python2Parser, { Python2Listener } from '../../../src/parser/python2';
 
+
+
 describe('Python2 Listener Tests', () => {
-  const example = `while (count < 9):`;
+  const example = `for i in range(5):\n    print(i)`;
   const parser = new Python2Parser();
 
   const parserTree = parser.parse(example);
 
-  test('Listener enterWhile', async () => {
+test('Listener enterWhile', async () => {
     let result = '';
     class MyListener extends Python2Listener {
-      enterWhile_stmt(ctx): void {
+      enterFor_stmt(ctx): void {
         result = ctx.getText().toLowerCase();
       }
     }
     const listenIfName: any = new MyListener();
 
     await parser.listen(listenIfName, parserTree);
-    expect(result).toBe("while(count<9):newline");
+    expect(result).toBe(`foriinrange(5):newlineindentprint(i)newlinededent`);
   });
 });
+
+
