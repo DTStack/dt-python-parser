ebb630f8709b50f9ff1fd1cddabad2a15675b965
chenfeng <chenfeng@dtstack.com>
2020-11-11 15:41:56 +0800

    test: add multiline output test

--- test/parser/python3/syntax.test.ts
+++ test/parser/python3/syntax.test.ts
@@ -2,6 +2,11 @@ import Python3Parser from '../../../src/parser/python3';
 
 describe('Python3 Syntax Tests', () => {
   const parser = new Python3Parser();
+  test('the print right example', () => {
+    const example = `print('abc')\nprint('abc')`;
+    const result = parser.validate(example);
+    expect(result.length).toBe(0);
+  });
 test('the first right example', () => {
     const example = `a = 10\nif a > 5:\n    print("a比5大")\nelse:\n    print("a比5小")`;
     const result = parser.validate(example);
@@ -18,9 +23,14 @@ test('python3 while example', () => {
     expect(result.length).toBe(0);
   });
 test('python3 wrong example', () => {
-    const example = `sum = 0\nnum = 1\nwhile num <= 100\n    sum += num\n    num += 1`;
+    const example = `print('abc')`;
+    const result = parser.validate(example);
+    expect(result.length).toBe(0);
+  });
+  test('python3 wrong example', () => {
+    const example = `print('abc'`;
     const result = parser.validate(example);
     expect(result.length).toBe(1);
-    expect(result?.[0]?.message).toBe(`missing ':' at ' '`);
+    expect(result?.[0]?.message).toBe(`no viable alternative at input 'print('abc''`);
   });
 });
