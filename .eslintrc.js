198a9c0f86a283a41cdf84fd274570f6f906a5d4
chenfeng <chenfeng@dtstack.com>
2020-11-20 15:23:34 +0800

    build: update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: ['google', 'plugin:prettier/recommended'],
+  extends: ['google', 'plugin:prettier/recommended', 'prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
