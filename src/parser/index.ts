a3e25037f5f417434de4121691241b65ad17f799
chenfeng <chenfeng@dtstack.com>
2020-11-24 16:44:23 +0800

    refactor: project entrance

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,34 +1,7 @@
-import Python2 from './python2';
-import Python3 from './python3';
+export { default as Python2 } from './python2';
+export { Python2Listener } from './python2';
+export { Python2Visitor } from './python2';
 
-export const ParserPython2 = inputBlock => {
-    try {
-        let resultArray;
-        const Python2Parser = new Python2();
-        if (Array.isArray(inputBlock)) {
-            resultArray = inputBlock.join('');
-        } else {
-            resultArray = inputBlock;
-        }
-        const result = Python2Parser.validate(resultArray);
-        return result[0];
-    } catch (e) {
-        return e;
-    }
-};
-
-export const ParserPython3 = inputBlock => {
-    try {
-        let resultArray;
-        const Python3Parser = new Python3();
-        if (Array.isArray(inputBlock)) {
-            resultArray = inputBlock.join('');
-        } else {
-            resultArray = inputBlock;
-        }
-        const result = Python3Parser.validate(resultArray);
-        return result[0];
-    } catch (e) {
-        return e;
-    }
-};
+export { default as Python3 } from './python3';
+export { Python3Listener } from './python3';
+export { Python3Visitor } from './python3';
