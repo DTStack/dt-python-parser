2a99219384c45f5b972895eff4258f17759e1384
chenfeng <chenfeng@dtstack.com>
2020-12-29 13:58:51 +0800

    pref: formatting code

--- test/parser/python3/listener.test.ts
+++ test/parser/python3/listener.test.ts
@@ -1,21 +1,24 @@
 import Python3Parser, { Python3Listener } from '../../../src/parser/python3';
 
-describe('Python3 Listener Tests', () => {
-  const example = `for i in range(5):\n    print(i)`;
-  const parser = new Python3Parser();
+describe('Python3 Listener Tests', (): void => {
+    const example = `import sys\nfor i in sys.argv:\n    print(i)`;
+    const parser = new Python3Parser();
 
-  const parserTree = parser.parse(example);
+    const parserTree = parser.parse(example);
 
-test('Listener enterWhile', async () => {
-    let result = '';
-    class MyListener extends Python3Listener {
-      enterFor_stmt(ctx): void {
-        result = ctx.getText().toLowerCase();
-      }
-    }
-    const listenIfName: any = new MyListener();
+    test('Listener enterWhile', async () => {
+        let result = '';
+        class MyListener extends Python3Listener {
+            enterImport_name(ctx): void {
+                result = ctx
+                    .getText()
+                    .toLowerCase()
+                    .match(/(?<=import).+/)?.[0];
+            }
+        }
+        const listenIfName: any = new MyListener();
 
-    await parser.listen(listenIfName, parserTree);
-    expect(result).toBe(`foriinrange(5):     print(i)))`);
-  });
+        await parser.listen(listenIfName, parserTree);
+        expect(result).toBe(`sys`);
+    });
 });
--- test/parser/python3/syntax.test.ts
+++ test/parser/python3/syntax.test.ts
@@ -1,42 +1,44 @@
 import Python3Parser from '../../../src/parser/python3';
 
 describe('Python3 Syntax Tests', () => {
-  const parser = new Python3Parser();
-  test('the print right example', () => {
-    const example = `print('abc')\nprint('abc')\n`;
-    const result = parser.validate(example);
-    expect(result.length).toBe(0);
-  });
-  test('the print wrong example', () => {
-    const example=`print 'abc'`; 
-    const result = parser.validate(example);
-    expect(result.length).toBe(1);
+    const parser = new Python3Parser();
+    test('the print right example', () => {
+        const example = `print('abc')\nprint('abc')\n`;
+        const result = parser.validate(example);
+        expect(result.length).toBe(0);
+    });
+    test('the print wrong example', () => {
+        const example = `! print('abc')`;
+        const result = parser.validate(example);
+        console.log(result);
+        expect(result.length).toBe(1);
     });
 
-test('the first right example', () => {
-    const example = `a = 10\nif a > 5:\n    print("a比5大")\nelse:\n    print("a比5小")`;
-    const result = parser.validate(example);
-    expect(result.length).toBe(0);
-  });
-test('python3 for example', () => {
-    const example = `for i in range(5):\n    print(i)`;
-    const result = parser.validate(example);
-    expect(result.length).toBe(0);
-  });
-test('python3 while example', () => {
-    const example = `sum = 0\nnum = 1\nwhile num <= 100:\n    sum += num\n    num += 1`;
-    const result = parser.validate(example);
-    expect(result.length).toBe(0);
-  });
-test('python3 wrong example', () => {
-    const example = `print('abc')`;
-    const result = parser.validate(example);
-    expect(result.length).toBe(0);
-  });
-  test('python3 wrong example', () => {
-    const example = `print('abc'`;
-    const result = parser.validate(example);
-    expect(result.length).toBe(1);
-    expect(result?.[0]?.message).toBe(`no viable alternative at input 'print('abc''`);
-  });
+    test('the first right example', () => {
+        const example = `! a = 10\nif a > 5:\n    print("a bigger than 5")\nelse:\n    print("a smaller than 5")`;
+        const result = parser.validate(example);
+        console.log();
+        expect(result.length).toBe(1);
+    });
+    test('python3 for example', () => {
+        const example = `import sys\nfor i in sys.argv:\n    print(i)`;
+        const result = parser.validate(example);
+        expect(result.length).toBe(0);
+    });
+    test('python3 while example', () => {
+        const example = `sum = 0\nnum = 1\nwhile num <= 100:\n    sum += num\n    num += 1`;
+        const result = parser.validate(example);
+        expect(result.length).toBe(0);
+    });
+    test('python3 wrong example', () => {
+        const example = `print('abc')`;
+        const result = parser.validate(example);
+        expect(result.length).toBe(0);
+    });
+    test('python3 wrong example', () => {
+        const example = `print('abc'`;
+        const result = parser.validate(example);
+        expect(result.length).toBe(1);
+        expect(result?.[0]?.message).toBe(`no viable alternative at input 'print('abc''`);
+    });
 });
